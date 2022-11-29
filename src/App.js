60125771e0dd62d8379419fff0f07082328e5657
joyfuldev <wings2132@naver.com>
2022-11-29 23:39:35 +0900

    redux practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Box from "./components/Box";
 
 function App() {
+  const count = useSelector((state) => state.count);
+  const id = useSelector((state) => state.id);
+  const password = useSelector((state) => state.password);
+  const dispatch = useDispatch();
+
+  const increase = () => {
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
+  };
+
+  const decrease = () => {
+    dispatch({ type: "DECREMENT", payload: { num: 2 } });
+  };
+
+  const login = () => {
+    dispatch({ type: "LOGIN", payload: { id: "bts", password: "0613" } });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {id}, {password}
+      </h1>
+      <h1>{count}</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={login}>Login</button>
+      <Box />
     </div>
   );
 }
